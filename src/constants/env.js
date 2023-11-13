bbbf8e8dedb8830c13ce5728060ed6a388700989
MiguelAcevedo <miguela@gigflow.co>
2023-11-13 16:28:24 -0500

    api url changed

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,2 +1,2 @@
-/* export const url_api = "https://colombian-reserve-server.azurewebsites.net/api" */
-export const URL_API = "http://localhost:4000/api"
+export const url_api = "https://colombian-reserve-server.azurewebsites.net/api"
+/* export const URL_API = "http://localhost:4000/api" */
